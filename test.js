4e7cf09f90f94ec3918bfeeb62b75546527693b8
Yunwoo <ywaidenchang@gmail.com>
2024-07-28 23:31:14 +0900

    24-07-28 11:31p.m.

--- test.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-axios.get(`https://www.gs1.org/services/verified-by-gs1/results?gtin=${String(data)}`)
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.log(error.message);
-});
